29603d82a0636f025b932bc0146cca7245b86448
Mariam <mariam1234ezz@gmail.com>
2026-03-15 06:47:33 +0200

    fix energy page build

--- src/app/energy/page.tsx
+++ src/app/energy/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic";
 'use client'
+export const dynamic = "force-dynamic";
 
 import Header from '@/components/Header'
 import { Card, StatCard, ProgressBar } from '@/components/Cards'
